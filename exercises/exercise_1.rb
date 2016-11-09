5d50504be44f53b90fb577470bde0ca5b217706b
Dane Sirois <dane_sirois@live.com>
2016-11-09 02:13:53 +0000

    Did a couple of the exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(name: "Vancouver", annual_revenue: 30000000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Saskatoon", annual_revenue: 3000000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Kelowna", annual_revenue: 1200000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count()
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Kamloops", annual_revenue: 15500, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Surey", annual_revenue: 130000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 14560000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womans_apparel: false)
+@mens_stores.each do |store|
+  put store.name
+  put store.annual_revenue  
+end
+
+@womens_stores = Store.where(womans_apparel: true, mens_apparel: false) 
+@womens_stores.where("annual_revenue < 10000000")
